a5aeab861c8ab85ad79f501236055d026ac742cc
Erinç <email": null>
2016-08-29 15:00:02 -0700

    not done

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    if @soda_machine[:sodas].include? soda_brand
+      @sodas[soda_brand]
+    else
+      nil
+    end
   end
 
   def sell(soda_brand)
+    #if @soda_machine[:sodas].include? soda_brand
+
   end
 
 end
+
+
